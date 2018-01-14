779a45a23b09df796adbfc691d2e5b8cee42ee64
Rodrigo Soldi <rodrigosoldi@icloud.com>
2018-01-14 19:53:26 -0200

    Initial Commit

---
+++ Chord/Chord.h
@@ -0,0 +1,19 @@
+//
+//  Chord.h
+//  Chord
+//
+//  Created by Rodrigo Soldi on 14/01/18.
+//  Copyright © 2018 Rodrigo Soldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chord.
+FOUNDATION_EXPORT double ChordVersionNumber;
+
+//! Project version string for Chord.
+FOUNDATION_EXPORT const unsigned char ChordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chord/PublicHeader.h>
+
+
